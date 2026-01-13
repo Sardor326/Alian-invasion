31cb0aae5a82eea30e5dd935a68c7d38f15562f0
Sardor326 <sardortoxirov013@gmail.com>
2026-01-13 18:20:47 +0500

    Delete raindrop_settings.py

--- raindrop_settings.py
+++
@@ -1,8 +0,0 @@
-class SettingsRain:
-    def __init__(self):
-        self.screen_width=1400
-        self.screen_height=1100
-        self.bg_color=(220, 230, 255)
-
-        #drop speed
-        self.raindrop_speed=2
